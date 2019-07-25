e33a020f50c3d98d45fe1fe8fcb0f1b14df2a379
Yisrael Grimes <yisraelgrimes@gmail.com>
2019-07-25 15:04:04 -0500

    init files

---
+++ index.js
@@ -0,0 +1,5 @@
+const chalk = require("chalk")
+
+module.exports = () => {
+	console.log(chalk.red('hello from child-1! I\'m managed in a separate repo!'))
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+const childOne = require("./index.js")
+
+childOne()
+
+const localPackageTwo = require("@monorepo-test/package-one")
+
+localPackageTwo()
